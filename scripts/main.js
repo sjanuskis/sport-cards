996215199201115d673866e3ea2582fdc4fc10df
Simonas <simonas.januskis@gmail.com>
2017-08-25 10:33:02 +0200

    Javascrip added to first card

---
+++ scripts/main.js
@@ -0,0 +1,44 @@
+var firstCardHeader = document.querySelector('.header p');
+firstCardHeader.textContent = 'David Rocastle';
+
+var factFile = document.querySelector('.left b');
+factFile.textContent = "FACT FILE";
+
+var detailsFirstLineLeft = document.querySelector('#firstLineLeft');
+detailsFirstLineLeft.textContent = "Year of Birth";
+
+var detailsFirstLineRight = document.querySelector('#firstLineRight');
+detailsFirstLineRight.textContent = "1967";
+
+var detailsSecondLineLeft = document.querySelector('#secondLineLeft');
+detailsSecondLineLeft.textContent = "Height (cm)";
+
+var detailsSecondLineRight = document.querySelector('#secondLineRight');
+detailsSecondLineRight.textContent = "173";
+
+var detailsThirdLineLeft = document.querySelector('#thirdLineLeft');
+detailsThirdLineLeft.textContent = "Weight (kg)";
+
+var detailsThirdLineRight = document.querySelector('#thirdLineRight');
+detailsThirdLineRight.textContent = "72";
+
+var detailsFourthLineLeft = document.querySelector('#fourthLineLeft');
+fourthLineLeft.textContent = "Goals for Arsenal";
+
+var detailsFourthLineRight = document.querySelector('#fourthLineRight');
+detailsFourthLineRight.textContent = "34";
+
+var detailsFifthLineLeft = document.querySelector('#fifthLineLeft');
+detailsFifthLineLeft.textContent = "International Caps";
+
+var detailsFifthLineRight = document.querySelector('#fifthLineRight');
+detailsFifthLineRight.textContent = "14";
+
+var bottomTextTop = document.querySelector('#bottomTextTop');
+bottomTextTop.textContent = "Top Signed from: N/A";
+
+var bottomTextMiddle = document.querySelector('#bottomTextMiddle');
+bottomTextMiddle.textContent = "Position: Midfield";
+
+var bottomTextEnd = document.querySelector('#bottomTextEnd');
+bottomTextEnd.textContent = "Squad No.: N/A";
