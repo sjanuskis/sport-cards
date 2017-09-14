d45662389c37e8cc2630c23d48a17a8b2e344f48
Simonas <simonas.januskis@gmail.com>
2017-09-14 16:22:26 +0300

    done, with couple bugs

--- scripts/main.js
+++ scripts/main.js
@@ -94,6 +94,10 @@ function printCard (name, yearOfBirth, height, weight, goalsForCurrentTeam, inte
     </div>
   </div>
   </div>
+  <div class="buttons">
+    <a href="#" id="hide">HIDE</a>
+    <a href="#" id="show">SHOW</a>
+  </div>
   `
 };
 
@@ -101,5 +105,90 @@ function printCard (name, yearOfBirth, height, weight, goalsForCurrentTeam, inte
 // let - https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Statements/let
 // const - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
 // var firstCardHeader = document.querySelector('.header p');
+
+const hide = document.getElementById("hide");
+const show = document.getElementById("show");
+const cardOne = document.getElementById("firstCard");
+
+hide.addEventListener("click", function(){
+  if (cardOne.style.display == 'none') {
+    cardOne.style.display = 'block';
+    hide.style.display = 'block';
+    show.style.display = 'none';
+  } else {
+    cardOne.style.display = 'none';
+    hide.style.display = 'none';
+    show.style.display = 'block';
+  }
+}, false);
+
+show.addEventListener("click", function(){
+  if (cardOne.style.display == 'none') {
+    cardOne.style.display = 'block';
+    hide.style.display = 'block';
+    show.style.display = 'none';
+  } else {
+    cardOne.style.display = 'none';
+    hide.style.display = 'none';
+    show.style.display = 'block';
+  }
+}, false);
+
+const hideTwo = document.getElementById("hideTwo");
+const showTwo = document.getElementById("showTwo");
+const cardTwo = document.getElementById("secondCard");
+
+hideTwo.addEventListener("click", function(){
+  if (cardTwo.style.display == 'none') {
+    cardTwo.style.display = 'block';
+    hideTwo.style.display = 'block';
+    showTwo.style.display = 'none';
+  } else {
+    cardTwo.style.display = 'none';
+    hideTwo.style.display = 'none';
+    showTwo.style.display = 'block';
+  }
+}, false);
+
+showTwo.addEventListener("click", function(){
+  if (cardTwo.style.display == 'none') {
+    cardTwo.style.display = 'block';
+    hideTwo.style.display = 'block';
+    showTwo.style.display = 'none';
+  } else {
+    cardTwo.style.display = 'none';
+    hideTwo.style.display = 'none';
+    showTwo.style.display = 'block';
+  }
+}, false);
+
+const hideThree = document.getElementById("hideThree");
+const showThree = document.getElementById("showThree");
+const cardThree = document.getElementById("thirdCard");
+
+hideThree.addEventListener("click", function(){
+  if (cardThree.style.display == 'none') {
+    cardThree.style.display = 'block';
+    hideThree.style.display = 'block';
+    showThree.style.display = 'none';
+  } else {
+    cardThree.style.display = 'none';
+    hideThree.style.display = 'none';
+    showThree.style.display = 'block';
+  }
+}, false);
+
+showThree.addEventListener("click", function(){
+  if (cardThree.style.display == 'none') {
+    cardThree.style.display = 'block';
+    hideThree.style.display = 'block';
+    showThree.style.display = 'none';
+  } else {
+    cardThree.style.display = 'none';
+    hideThree.style.display = 'none';
+    showThree.style.display = 'block';
+  }
+}, false);
+
 
 };
